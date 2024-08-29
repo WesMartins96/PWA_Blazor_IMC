2862ea4523f9600b9a767bbb1aa3c523c6a58c17
Wesley Martins <wesley.mlopes@castgroup.com>
2024-08-29 11:57:38 -0300

    Adicionar arquivos de projeto.

---
+++ AppState.cs
@@ -0,0 +1,21 @@
+﻿using Imc.Models;
+
+namespace Imc
+{
+    public static class AppState
+    {
+        //lista de calculos de imc
+        public static List<ImcModel> History { get; set; } = new();
+
+        //manipulação de eventos
+        public static event EventHandler<EventArgs> OnChanged;
+
+
+        //evento para notificar um histórico novo
+        public static void NotifyChanges(object? sender, EventArgs args)
+        {
+            OnChanged.Invoke(sender, args);
+        }
+
+    }
+}
